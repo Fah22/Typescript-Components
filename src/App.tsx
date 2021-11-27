0810e6e1d6467f655d55969a90fdfb6322f91454
Fadziso Gabrielle <fadziso@novodigital.biz>
2021-11-27 20:17:20 +0200

    Modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, ReactNode, } from 'react';
 import './App.css';
 
 // Conventional Props
@@ -8,16 +8,52 @@ const Heading = ({ title }: { title: string }) => {
     )
 }
 
-const HeadingWithContent = ({ children }: { children: string }) => {
+const HeadingWithContent = ({ children }: { children: ReactNode }): ReactElement => {
   return (
     <h1>{children}</h1>
   )
 }
 
+// default props/ defaultProps
+const defaultContainerProps = {
+  heading: <strong>My heading</strong>
+}
+
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps;
+
+const Container = ({ heading, children }: ContainerProps): ReactElement => {
+  return (
+    <div><h1>{heading}</h1></div>
+  )
+}
+
+// functional props
+const TextWithNumber = ({ children }: { children: (num: number) => ReactNode }) => {
+    const [state, setState] = React.useState<number>(1);
+
+    return (
+      <div>
+        <div>
+          {children(state)}
+        </div>
+        <div>
+          <button onClick={() => setState(state + 1)}>Add</button>
+        </div>
+      </div>
+    )
+}
+
+Container.defaultProps = defaultContainerProps;
+
 const App = () => {
   return (
     <div>
       <Heading title="Hello There" />
+      <HeadingWithContent><strong>hi</strong></HeadingWithContent>
+      <Container>hi!</Container>
+      <TextWithNumber>
+        {(num: number) => <div>Today's number is {num} </div>}
+      </TextWithNumber>
     </div>
   );
 }
